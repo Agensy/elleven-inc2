6f448422cb776bb1a948ac875e8fb51a7c060450
Tiagogomezdigital <contatotiagogomezdigital@gmail.com>
2025-06-29 09:28:11 -0300

    fix: Correção de importações para resolver problema no V0
    - Alterada importação de barrel export para importação direta
    - Criado exemplo simples para V0 com importações diretas
    - Resolvido problema 'EmpreendimentoPage cannot be found'
    - Tipagem correta dos dados do empreendimento

--- app/obsidian-componentizado/page.tsx
+++ app/obsidian-componentizado/page.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next"
-import { EmpreendimentoPage } from "@/components/empreendimento"
+import EmpreendimentoPage from "@/components/empreendimento/EmpreendimentoPage"
 import { JadeEmpreendimentoData } from "@/lib/types/jade-template"
 
 // Dados do Obsidian usando a estrutura componentizada
